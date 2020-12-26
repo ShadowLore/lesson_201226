ba1b51e704392eaefa66c613ead4b5165d8ac4cd
nfsri <nfsrivals757@gmail.com>
2020-12-26 09:30:40 +0300

    ito

--- ito_4.py
+++ ito_4.py
@@ -1,7 +1,3 @@
-#  f = open('data/fairy_tail.txt', 'r', encoding='utf-8')
-#  content = f.read()
-#  f.close()
-
 with open('data/fairy_tail.txt', 'r', encoding='utf-8') as f:
     content = f.read()
 
@@ -15,19 +11,16 @@ print(f'слов в тексте {text_items_cnt}')
 
 text_items_counter = {}
 for el in text_items:
-    el = el.lower()
-    if el not in text_items_counter:
-        text_items_counter[el] = 0
-    text_items_counter[el] += 1
+   el = el.lower().strip(',.')
+   if el not in text_items_counter:
+         text_items_counter[el] = 0
+         text_items_counter[el] += 1
 
 print(f'уникальных слов в тексте {len(text_items_counter)}')
 
-text_items_counter_ordered = dict(sorted(text_items_counter.items(), key=lambda x: x[1], reverse=True))
+text_items_counter_ordered = dict(sorted(text_items_counter.items(), key=lambda x: x[1], reverse=True)[:100])
+print(f'TOP-{len(text_items_counter_ordered)}')
 print(text_items_counter_ordered)
-
-# print(text_items_counter.keys())
-# print(text_items_counter.values())
-# print(text_items_counter.items())
 
 
 
